752e37af68663d8a40c26d49dbee539b4b6ef512
Jumcee <futhmah456@gmail.com>
2024-09-25 00:31:54 +0100

    stylus

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,77 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
+use alloc::{string::String, vec::Vec};
+use stylus_sdk::{alloy_primitives::U256, prelude::*, msg, storage::StorageMap};
 
-// Allow `cargo stylus export-abi` to generate a main function.
-#![cfg_attr(not(feature = "export-abi"), no_main)]
-extern crate alloc;
-
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
-
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
-    }
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Task {
+    id: U256,
+    description: String,
+    reward: U256,
+    creator: Address,
+    assignee: Option<Address>,
+    completed: bool,
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
-#[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+#[stylus_sdk::contract]
+mod task_marketplace {
+    use super::*;
+
+    #[storage]
+    struct TaskMarketplace {
+        tasks: StorageMap<U256, Task>,
+        task_count: U256,
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
+    #[external]
+    impl TaskMarketplace {
+        pub fn create_task(&mut self, description: String, reward: U256) -> Result<U256, Vec<u8>> {
+            let task_id = self.task_count.add(1);
+            let task = Task {
+                id: task_id,
+                description,
+                reward,
+                creator: msg::sender(),
+                assignee: None,
+                completed: false,
+            };
+            self.tasks.insert(task_id, task);
+            self.task_count = task_id;
+            Ok(task_id)
+        }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
+        pub fn accept_task(&mut self, task_id: U256) -> Result<(), Vec<u8>> {
+            let mut task = self.tasks.get(task_id).ok_or("Task not found")?;
+            if task.assignee.is_some() {
+                return Err("Task already assigned".into());
+            }
+            task.assignee = Some(msg::sender());
+            self.tasks.insert(task_id, task);
+            Ok(())
+        }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
+        pub fn complete_task(&mut self, task_id: U256) -> Result<(), Vec<u8>> {
+            let mut task = self.tasks.get(task_id).ok_or("Task not found")?;
+            if task.assignee != Some(msg::sender()) {
+                return Err("Not assigned to this task".into());
+            }
+            if task.completed {
+                return Err("Task already completed".into());
+            }
+            task.completed = true;
+            self.tasks.insert(task_id, task.clone());
+            
+            // Transfer reward to assignee
+            msg::send(task.assignee.unwrap(), task.reward);
+            Ok(())
+        }
 
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+        pub fn get_task(&self, task_id: U256) -> Result<Task, Vec<u8>> {
+            self.tasks.get(task_id).ok_or("Task not found".into())
+        }
+
+        pub fn get_all_tasks(&self) -> Vec<Task> {
+            (1..=self.task_count)
+                .filter_map(|id| self.tasks.get(id))
+                .collect()
+        }
     }
 }
